106662fd5341b0379d2026aa9869cb3342731e45
ShamimHossain1 <shamimweb78@gmail.com>
2025-03-05 00:06:57 -0500

    logout

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from '../../firebase';
 
 
 
 const LogIn = () => {
+    const [user, setUser] = useState(null);
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
             .then((result) => {
                 console.log(result.user);
+                setUser(result.user);
+            })
+            .catch((error) => {
+                console.log(error.message);
+                setUser(null);
+            });
+    }
+
+    const handleSignout = () => {
+        signOut(auth)
+            .then(() => {
+                setUser(null);
             })
             .catch((error) => {
                 console.log(error.message);
             });
+
     }
+
 
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Login With Google</button>
+            {user ?
+                <button onClick={handleSignout}>Sign Out</button> : <button onClick={handleGoogleSignIn}>Login With Google</button>
+            }
+            {
+                user && <div>
+                    <h1>Welcome, {user.displayName}</h1>
+                </div>
+            }
+
             <button>Login With Facebook </button>
         </div>
     );
