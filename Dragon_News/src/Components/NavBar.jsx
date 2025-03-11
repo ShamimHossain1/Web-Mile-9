c622b511d6641037e9f7818b3bfaee75979ca421
ShamimHossain1 <shamimweb78@gmail.com>
2025-03-11 03:23:45 -0400

    nav bar

--- Dragon_News/src/Components/NavBar.jsx
+++ Dragon_News/src/Components/NavBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import user from '../assets/user.png'
 
 const NavBar = () => {
     return (
-        <div>
-            
+        <div className='flex justify-between items-center '>
+            <div></div>
+            <div className='nav flex gap-5 items-center my-8' >
+
+                <Link to='/'><p className='text-[#706F6F] text-xl'>Home</p></Link>
+                <Link to='/news'><p className='text-[#706F6F] text-xl'>About</p></Link>    
+                <Link to='/login'><p className='text-[#706F6F] text-xl'>Career</p></Link>
+
+            </div>
+            <div className="login flex gap-5 items-center">
+
+                <img src={user}/>
+                <button className='bg-[#403F3F] text-white px-4 text-xl font-semibold py-2'>Login</button>
+                <button className='bg-[#403F3F] text-white px-4 text-xl font-semibold py-2 '>Log Out</button>
+            </div>
+
         </div>
     );
 };
--- Dragon_News/src/layouts/HomeLayout.jsx
+++ Dragon_News/src/layouts/HomeLayout.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Header from '../Components/Header';
 import LatestNews from '../Components/LatestNews';
+import NavBar from '../Components/NavBar';
 
 const HomeLayout = () => {
     return (
         <div>
             <header>
                 <Header />
-                <section className='w-9/12 mx-auto'>
+                <section className='w-10/12 mx-auto'>
                     <LatestNews />
                 </section>
             </header>
-            <nav></nav>
+            <nav className='w-10/12 mx-auto'>
+                <NavBar></NavBar>
+            </nav>
             <aside></aside>
             <main></main>
             <footer></footer>
